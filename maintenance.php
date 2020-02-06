47c56bb04e431b416c9b24aabc028f11cdb60813
Felipe Gomes <developer.felipegomes@gmail.com>
2020-02-06 07:32:51 -0300

    Add files.

---
+++ maintenance.php
@@ -0,0 +1,37 @@
+<?php
+	http_response_code(503);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Estamos em manutenção</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+		.centered {
+			text-align: center;
+		}
+
+		body {
+			background-color: #f0eef3;
+		}
+		
+		h1 {
+			font-family: monospace;
+		}
+	</style>
+</head>
+<body>
+	<div class="centered">
+		<!-- Thanks to Pamela Giani for the image -->
+		<!-- https://dribbble.com/shots/5310986-Maintenance-Page#shot-description -->
+		<img src="maintenance.gif" alt="Estamos manutenção" title="Estamos em manutenção">
+		<br>
+		<br>
+		<h1>Estamos trabalhando em melhorias e voltaremos logo :)</h1>
+		<br>
+		<br>
+		<h1>-- 503 --</h1>		
+	</div>
+</body>
+</html>
